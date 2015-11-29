ae8d7bfaee2026580a02c3819f0327ce65f2b273
teru03 <terusun@gmail.com>
2015-11-29 14:57:13 +0000

    mongodb update

--- server.js
+++ server.js
@@ -21,36 +21,73 @@ var DbCtrl = require('./dbctrl.js');
 var router = express();
 var server = http.createServer(router);
 var io = socketio.listen(server);
-var dbctrl = new DbCtrl;
 
 router.use(express.static(path.resolve(__dirname, 'client')));
 
 io.on('connection', function (socket) {
+  
+  //searchedworddbへのコネクト
+  var dbctrl = new DbCtrl();
+  
 
-  socket.on('wordsearch', function (msg) {
+  socket.on('connect',function(){
+    //最新の5件を検索
+    dbctrl.find(5);
+    //0.検索結果
+    dbctrl.on('find',function(words){
+      console.log('first find',words);
+      //0-1.クライアントに検索ワード列を
+//      socket.broadcast.emit('transwordlist',words);  
+      socket.emit('transwordlist',words);  
+    })
+    
+  })  
+
+  //2.翻訳処理実行
+  socket.on('wordtrans', function (msg) {
+    //2-1.トークンの取得
     var MicrosoftTranslator = require('./microsofttranslator.js');
 //    console.log("MicrosoftTranslator = ", MicrosoftTranslator);
 //    console.log("input text = ", msg);
-    var data = {'word':msg};
-    socket.emit('wordsearchresult', data );
-    
-    var transctrl = new MicrosoftTranslator.MicrosoftTranslator("your id","your client_secret");
+    var transctrl = new MicrosoftTranslator.MicrosoftTranslator("your id","your secret");
     transctrl.adm.getToken();
-    
+
+    //2-2.トークン取得成功
     transctrl.adm.on( 'token', function(token){
 //      console.log("token = ", token);
       var tokendata = {'token': token };
+      //トークンをクライアントへ
       socket.emit('gettoken', tokendata );
 
     });
-    
+
+    //エラーハンドラ  
     transctrl.adm.on('error', function(e){
       console.log("error = ", e);
     });
 
   });
 
+  //4.結果をmongodbへ保存
+  socket.on('result',function(word){
+    console.log("result = ", word);
+    dbctrl.insert(word);
+    
+    //4-1.検索結果
+    dbctrl.on('insert',function(words){
+      //4-1-1.クライアントに検索ワード列を
+//      socket.broadcast.emit('transwordlist',words);  
+      socket.emit('transwordlist',words);  
+    })
+  });
+
+  //切断時の処理  
+  socket.on("disconnect", function () {
+    dbctrl.disconnect();
+  });  
+
 });
+
 
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){

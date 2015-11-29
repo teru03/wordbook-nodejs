0c4000eb691b7f2af7bd92a3ab04eada2442d139
teru03 <terusun@gmail.com>
2015-11-29 14:49:56 +0000

    mongodb

--- dbctrl.js
+++ dbctrl.js
@@ -2,34 +2,38 @@ var mongoose = require('mongoose');
 var util = require('util');
 var events = require('events');
 
-var db;
+var db = null;
 
 var DbCtrl = function() {
-    db = mongoose.connect('mongodb://localhost/searchedword');
-
-    //検索されたワードのスキーマを宣言。
-    var WordSchema = new mongoose.Schema(
-        {
-            word:{type:String},
-            count:{type:Number}
-        }
-    );
-    //スキーマからモデルを生成。
-    db.model('word',WordSchema);
+    if( !db ){
+        db = mongoose.connect('mongodb://0.0.0.0:27017/searchedword');
+    
+        //検索されたワードのスキーマを宣言。
+        var WordSchema = mongoose.Schema(
+            {
+                word:{type:String},
+                count:{type:Number},
+                update:{type:Date}
+            }
+        );
+        //スキーマからモデルを生成。
+        db.model('word',WordSchema);
+    }
     
 };
 
 util.inherits(DbCtrl, events.EventEmitter);
 
-DbCtrl.prototype.find = function() {
+DbCtrl.prototype.find = function(n) {
 	var self = this;
     var Word = db.model('word');
-    Word.find({} , function(err, docs) {
+    Word.find().sort({update:-1}).limit({limit:n}).exec( function(err, docs) {
         if(err){
             console.log(err);
             self.emit('err', err);
         }
         else{
+//            console.log(docs);
             self.emit('find', docs);
         }
     });    
@@ -43,22 +47,33 @@ DbCtrl.prototype.insert = function(msg) {
     var Word = db.model('word');
     Word.find({word:msg} , function(err, docs) {
         if( docs.length >= 1){
-            docs[0].doc.count++;
-            self.emit('insert', docs[0].doc);
+            docs[0].count++;
+            docs[0].update = new Date();
+            docs[0].save(function(err) {
+                if (err) { console.log(err); }
+                self.emit('err', msg);
+            });
         }
         else{
             var word = new Word;
             word.word = msg;
             word.count = 1;
+            word.update = new Date();
             word.save(function(err) {
                 if (err) { console.log(err); }
                 self.emit('err', msg);
             });
-            self.emit('insert', word);
-            
+
         }
+        db.model('word').find().sort({update:-1}).limit({limit:5}).exec(function(err,searcheddocs){
+            self.emit('insert', searcheddocs);
+        });
     });    
 
+}
+
+DbCtrl.prototype.disconnect = function() {
+    mongoose.disconnect();
 }
 
 module.exports = DbCtrl;
